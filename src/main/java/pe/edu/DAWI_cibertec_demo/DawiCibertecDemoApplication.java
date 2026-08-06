a74c3780289f491b508f620dc6895562f04a85a4
Pier Olivares <olivares.3647@gmail.com>
2026-08-06 06:13:24 -0500

    first commit / pom y properties realizados

---
+++ src/main/java/pe/edu/DAWI_cibertec_demo/DawiCibertecDemoApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.DAWI_cibertec_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiCibertecDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiCibertecDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/DAWI_cibertec_demo/DawiCibertecDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.DAWI_cibertec_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiCibertecDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
