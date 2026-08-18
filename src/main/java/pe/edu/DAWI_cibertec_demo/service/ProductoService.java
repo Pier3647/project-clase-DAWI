ecf3feb75340d4f60c22db5dc06caaa6d24e7481
Pier Olivares <olivares.3647@gmail.com>
2026-08-17 21:27:44 -0500

    tema 07: Spring Secutiry

--- src/main/java/pe/edu/DAWI_cibertec_demo/service/ProductoService.java
+++ src/main/java/pe/edu/DAWI_cibertec_demo/service/ProductoService.java
@@ -3,21 +3,27 @@ package pe.edu.DAWI_cibertec_demo.service;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.transaction.Transactional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import pe.edu.DAWI_cibertec_demo.model.Producto;
 import pe.edu.DAWI_cibertec_demo.repository.ProductoRepository;
+import pe.edu.DAWI_cibertec_demo.util.FormatoUtil;
 
 import java.util.List;
 
 @Service
 public class ProductoService {
     private final ProductoRepository productoRepository;
+    private final FormatoUtil formatoUtil;
 
     @PersistenceContext
     private EntityManager em;
 
-    public ProductoService(ProductoRepository productoRepository) {
+
+    public ProductoService(ProductoRepository productoRepository, FormatoUtil formatoUtil) {
         this.productoRepository = productoRepository;
+        this.formatoUtil = formatoUtil;
     }
 
     //Insercion por lotes (batch insert)
@@ -25,6 +31,7 @@ public class ProductoService {
     public void registrarLote(List<Producto> productos){
         int i = 0;
         for (Producto producto : productos){
+            producto.setNombre(formatoUtil.capitalizar(producto.getNombre()));
             em.persist(producto);
             i++;
             if(i % 10 == 0){
@@ -32,6 +39,8 @@ public class ProductoService {
                 em.clear();
             }
         }
+        em.flush();
+        em.clear();
     }
 
     // Fetching con Entity Graph (solo nombre y precio)
@@ -39,6 +48,15 @@ public class ProductoService {
         return em.createQuery("SELECT p FROM Producto p", Producto.class)
                 .setHint("org.hibernate.fetchSize", 5)
                 .getResultList();
+    }
+
+    //Buscar productos + paginado
+
+    public Page<Producto> buscarPorNombre(String texto, Pageable pageable){
+        if (texto == null || texto.trim().isEmpty()) {
+            return productoRepository.findAll(pageable);
+        }
+        return productoRepository.findByNombreContainingIgnoreCase(texto.trim(), pageable);
     }
 
 
